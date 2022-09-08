7eee2845729af3b0bdfab73eb02e338358aee953
ugbo chinedu <o.ugbochinedu@yahoo.com>
2022-09-08 10:09:57 -0700

    quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,8 +7,6 @@
 
 int main(void)
 {
-	char n[] = "and that piece of art is useful\" - Dora Korpar,
-2015-10-19";
-	fprintf(stderr, "%s\n", n);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
