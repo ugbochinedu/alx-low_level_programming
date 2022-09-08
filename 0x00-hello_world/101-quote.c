65b9c16711a0ce7a15ea0f32a13235f451c4a07e
ugbo chinedu <o.ugbochinedu@yahoo.com>
2022-09-08 09:24:52 -0700

    6-size.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,21 +1,12 @@
 #include <stdio.h>
 /**
+ * main - Entry point
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
-
-{
-
-	char n[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-
-			fprintf(stderr, "%s\n", n);
-			return (1);
-
+{	
+	char n[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";	    fprintf(stderr, "%s\n", n);
+	return (1);
 }
