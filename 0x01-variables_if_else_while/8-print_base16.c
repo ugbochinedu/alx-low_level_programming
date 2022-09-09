5adb1a9428f4d1991f6db573ea7bf40ab7012714
ugbo chinedu <o.ugbochinedu@yahoo.com>
2022-09-09 06:01:36 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - numbers below 16
+ *
+ * Return: Always (success)
+ */
+
+int main(void)
+{
+	char n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+
+	for (n = 'a'; n <= 'f'; n++)
+	{
+		putchar(n)
+	}
+
+	putchar('\n');
+
+	return (0);
+}
