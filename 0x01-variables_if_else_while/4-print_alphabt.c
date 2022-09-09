a4dec38f924f2dcc611fc84760142c4fb394636d
ugbo chinedu <o.ugbochinedu@yahoo.com>
2022-09-09 04:45:38 -0700

    except e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - print all alphabets except e and q
+ *
+ * Return: always (sucess)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'e' && c != 'q')
+		{
+			putchar(c);
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
