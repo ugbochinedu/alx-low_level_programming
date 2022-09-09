3ca72f68fb3cfd2d32263e0a4cbd61896b1da4d8
ugbo chinedu <o.ugbochinedu@yahoo.com>
2022-09-09 04:33:52 -0700

    uper and lower case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main -  prints lower and upper case alphabet
+ *
+ * Return: Always (success)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
