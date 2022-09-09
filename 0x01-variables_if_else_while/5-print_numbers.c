b08fd290687a58f4026b7337b4c7b1a40e171e0a
ugbo chinedu <o.ugbochinedu@yahoo.com>
2022-09-09 04:55:29 -0700

    numbers less than 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - print number from 0 to 9
+ *
+ * Return: Always (success)
+ */
+
+int main(void)
+{
+	char n;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar(n);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
