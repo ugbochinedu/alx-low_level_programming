09fddbda33ab7964095f1fddff5cec9aadfe5df3
ugbo chinedu <o.ugbochinedu@yahoo.com>
2022-09-13 04:40:44 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * print_last_digit - last digit
+ * @n: number being tested
+ * Return: Always 0.
+ */
+
+int print_last_digit(int n)
+{
+	n = n % 10;
+
+	if (n < 0)
+	{
+		n = -n;
+		_putchar (n + '0');
+	}
+
+	return (n);
+}
