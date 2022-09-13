ae4ca26ca0da0172b27806358dc1bd4718cd8c83
ugbo chinedu <o.ugbochinedu@yahoo.com>
2022-09-13 10:22:23 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - computes and prints the sum
+ *
+ * Return: always 0.
+ */
+
+int main(void)
+{
+	int i, res;
+
+	for (i = 0; i < 1024; i++)
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			res += i;
+			printf("%d\n", res);
+		}
+	return (0);
+}
