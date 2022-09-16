27510dc8e8cbd3acd4b8bd5c7ba9c51c2d863f48
ugbo chinedu <o.ugbochinedu@yahoo.com>
2022-09-16 05:04:40 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,15 +8,34 @@
 
 void print_number(int n)
 {
-	unsigned int x;
+	unsigned int x, i = 1, y;
 
-	x = n;
 	if (n < 0)
 	{
-		_putchar(45);
-		x = -n;
+		_putchar('-');
+		y = -n;
+		x = y;
 	}
-	if (x / 10)
-		print_number(x / 10);
-	_putchar((x % 10) + '0');
+	else
+	{
+		y = n;
+		x = y;
+	}
+	if (n == 0)
+		_putchar('0');
+	else
+	{
+		while (x > 9)
+		{
+			i *= 10;
+			x /= 10;
+		}
+		while (i > 1)
+		{
+			_putchar((y / i) + '0');
+			y %= i;
+			i /= 10;
+		}
+		_putchar(y + '0');
+	}
 }
