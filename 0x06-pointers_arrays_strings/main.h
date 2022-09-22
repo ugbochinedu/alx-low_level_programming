49c307e7785d1e775a61971ee13d875d0e2e35b0
ugbo chinedu <o.ugbochinedu@yahoo.com>
2022-09-22 01:57:44 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,10 +3,10 @@
 
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
-char *_strncat(char *dest, chat *src, int n);
+char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
-void reverse _array(int *a, int n);
+void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
