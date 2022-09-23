18dc6be62c956da2cb3ed670150994a4de05f30f
ugbo chinedu <o.ugbochinedu@yahoo.com>
2022-09-23 03:36:18 -0700

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -37,7 +37,7 @@ void print_line(char *c, int s, int l)
  * @size: The number of byte
  * Return: 0
  */
-
+/*
 void print_buffer(char *b, int size)
 {
 	int i;
@@ -57,4 +57,41 @@ void print_buffer(char *b, int size)
 	}
 	if (size == 0)
 		putchar('\n');
+
+}
+*/
+
+void print_buffer(char *b, int size)
+{
+	int byte, index;
+
+	for (byte = 0; byte < size; byte += 10)
+	{
+		printf("%08x: ", byte);
+
+		for (index = 0; index < 10; index++)
+		{
+			if ((index + byte) >= size)
+				printf(" ");
+			else
+				printf("%02x", *(b  + index + byte));
+			if ((index % 2) != 0 && index != 0)
+				printf(" ");
+		}
+		for (index = 0; index < 10; index++)
+		{
+			if ((index + byte) >= size)
+				break;
+			else if (*(b + index + byte) >= 31 &&
+				*(b + index + byte) <= 126)
+				printf("%c", *(b + index + byte));
+			else
+				printf(".");
+		}
+		if (byte >= size)
+			continue;
+		printf("\n");
+	}
+	if (size <= 0)
+		printf("\n");
 }
