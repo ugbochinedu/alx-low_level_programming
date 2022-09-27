fbc93d0d8c1027ae87eefba8fe5f118e26582bee
ugbo chinedu <o.ugbochinedu@yahoo.com>
2022-09-27 10:55:35 -0700

    main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,17 +1,6 @@
-#ifndef main.h
-#define main.h
-
-/**
- * File: main.h
- * Auth: Fitdaus
- * desc: Header file containing prototypes for all  functions
- */
-
-#include <stddef.h>
-
-int_putchar(char c);
-void *memset(void *s, int c, size_t n);
-void *memcpy(void *dest, const void *src, size_t n);
+int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
@@ -19,5 +8,3 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
-
-#endif
