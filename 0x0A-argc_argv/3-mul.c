88f32a48fd6949a837dac7c60e8a232ddd8f01a8
ugbo chinedu <o.ugbochinedu@yahoo.com>
2022-09-30 10:47:28 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - programs that prints products
+ * @argc: number of command line arguments
+ * @argv: Array name
+ * Return: 1 or 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j;
+
+	if (argc == 1 || argc == 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		j = 1;
+
+		for (i = 1; i < 3; i++)
+			j *= atoi(argv[i]);
+		printf("%d\n", j);
+	}
+	return (0);
+}
