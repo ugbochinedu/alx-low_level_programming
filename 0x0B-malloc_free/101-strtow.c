66d07bbd51ce1dadc67b401fed9dc0c8d9b0c8fa
ugbo chinedu <o.ugbochinedu@yahoo.com>
2022-10-03 05:28:27 -0700

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -65,6 +65,9 @@ char **strtow(char *str)
 	words = count_words(str);
 	if (words == 0)
 		return (NULL);
+	strings = malloc(sizeof(char *) * (words + 1));
+	if (strings == NULL)
+		return (NULL);
 
 	for (w = 0; w < words; w++)
 	{
