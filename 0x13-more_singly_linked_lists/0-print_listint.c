a668e82b0026441fc567440f13d5914bf31d5056
ugbo chinedu <o.ugbochinedu@yahoo.com>
2022-10-25 02:23:19 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,17 +9,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t n = 0;
+	size_t nodes = 0;
 
-	if (h == NULL)
+	while (h)
 	{
-		return (0);
-	}
-	while (h != NULL)
-	{
+		nodes++;
 		printf("%d\n", h->n);
 		h = h->next;
 		printf("\n");
 	}
-	return (n);
+	return (nodes);
 }
